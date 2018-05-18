8a91b3ba23608e5849487d1f5b04dc3da9431e81
Vikas Ahuja <vahuja4@sapient.com>
2018-05-18 12:30:36 +0530

    commit add new file retail domain

---
+++ RetailDomain/RetailDomainController.swift
@@ -0,0 +1,15 @@
+//
+//  RetailDomainController.swift
+//  RetailDomain
+//
+//  Created by Vikas Ahuja on 5/18/18.
+//  Copyright © 2018 Vikas Ahuja. All rights reserved.
+//
+
+import Foundation
+
+class RetailDomainController {
+    
+    //This is just the test controller for retail Domain
+    
+}
